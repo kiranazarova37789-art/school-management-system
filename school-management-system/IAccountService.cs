3554672384d48a9b7cba1c2e763d3dbb6a828a1f
Kira Nazarova <nazarovakira789@gmail.com>
2026-09-01 21:53:57 +0300

    AccountControoler + AccountServise + IAccountServise

---
+++ school-management-system/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using school_management_system.DbModels;
+
+namespace school_management_system
+{
+    public interface IAccountService
+    {
+        Task<User?> LoginAcc(
+            string name,
+            string password
+        );
+
+        Task<bool> RegistrationAcc(
+            string name,
+            string password
+        );
+    }
+}
---
+++ school-management-system/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace school_management_system.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введите логин")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [StringLength(100, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Повторите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
